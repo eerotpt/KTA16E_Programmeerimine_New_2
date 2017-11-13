d30781f233ec200f9c9963201a45a94f632b8538
eerotpt <eero.sakson@tptlive.ee>
2017-11-13 11:48:43 +0200

    first_commit

---
+++ 01_Dice_Roller/01_Dice_Roller/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Dice_Roller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomNum = new Random();
+
+            Console.WriteLine($"1d6: { randomNum.Next(1, 7)}");
+            Console.WriteLine($"1d6: { randomNum.Next(1, 7)}");
+            Console.WriteLine($"1d6: { randomNum.Next(1, 7)}");
+            Console.WriteLine($"1d8:    { randomNum.Next(1, 9)}");
+            Console.WriteLine($"1d8:    { randomNum.Next(1, 9)}");
+
+            Console.WriteLine();
+            Console.WriteLine($"Roll total: 20");
+
+            Console.WriteLine();
+            Console.Write("> ");
+
+            Console.ReadLine();
+
+        }
+    }
+}
